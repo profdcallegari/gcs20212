b3a0df28cb35ea1cad1b3dc3c2f852ece17c0cc2
Prof. Daniel Callegari <43687162+profdcallegari@users.noreply.github.com>
2021-08-27 18:05:04 -0300

    Create App.java
    Meu primeiro arquivo fonte java adicionado.

---
+++ App.java
@@ -0,0 +1,11 @@
+class App
+{
+   public static void main (String[] args)
+   {
+      System.out.println("Ola turma de 2021/2 !"); 
+     
+     
+      System.out.println("Fim do programa."); 
+   }
+  
+}
