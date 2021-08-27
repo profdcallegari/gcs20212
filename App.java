1e00fbb2df9df9a3a528acee6e4b84ab8c927c89
Prof. Daniel Callegari <43687162+profdcallegari@users.noreply.github.com>
2021-08-27 18:07:47 -0300

    Update App.java

--- App.java
+++ App.java
@@ -1,11 +1,11 @@
 class App
 {
+   // Procedimento de entrada
    public static void main (String[] args)
    {
-      System.out.println("Ola turma de 2021/2 !"); 
-     
-     
-      System.out.println("Fim do programa."); 
+      System.out.println("Ola, turma de 2021/2 !"); 
+      System.out.println("Fim do Programa."); 
    }
   
+   // Autor: Daniel Callegari 2021
 }
